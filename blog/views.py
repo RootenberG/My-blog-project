5afcd3ce44fca8a1b7f99b056e2ee974ed232c00
RootenberG <savichevdenis244@gmail.com>
2019-11-26 21:53:25 +0200

    used black for pretty format

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,12 @@ from .forms import EmailPostForm, CommentForm
 from django.http import HttpResponse
 from django.core.mail import send_mail
 
+
 def post_share(request, post_id):
     # Получение статьи по идентификатору.
-    post = get_object_or_404(Post, id=post_id, status='published')
+    post = get_object_or_404(Post, id=post_id, status="published")
     sent = False
-   
+
     # Форма была отправлена на сохранение.
     form = EmailPostForm(request.POST)
     if form.is_valid():
@@ -19,32 +20,43 @@ def post_share(request, post_id):
         # ... Отправка электронной почты.
         post_url = request.build_absolute_uri(post.get_absolute_url())
         subject = '{} ({}) recommends you reading "\
-            {}"'.format(cd['name'], cd['email'], post.title)
+            {}"'.format(
+            cd["name"], cd["email"], post.title
+        )
         message = 'Read "{}" at {}\n\n{}\'s comments:\
-            {}'.format(post.title, post_url, cd['name'], cd['comments'])
-        send_mail(subject, message, 'savichevdenis244@gmail.com', [cd['to']])
+            {}'.format(
+            post.title, post_url, cd["name"], cd["comments"]
+        )
+        send_mail(subject, message, "savichevdenis244@gmail.com", [cd["to"]])
         sent = True
-        
+
     else:
         form = EmailPostForm()
-        return render(request, 'blog/post/share.html',
-                        {'post': post, 'form': form,'sent': sent})
+        return render(
+            request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+        )
 
 
 class PostListView(ListView):
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 3
-    template_name = 'blog/post/list.html'
+    template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
-                             publish__month=month, publish__day=day)
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status="published",
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
     # Список активных комментариев для этой статьи.
     comments = post.comments.filter(active=True)
     new_comment = None
-    if request.method == 'POST':
+    if request.method == "POST":
         # Пользователь отправил комментарий.
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -52,11 +64,17 @@ def post_detail(request, year, month, day, post):
             new_comment = comment_form.save(commit=False)
             # Привязываем комментарий к текущей статье.
             new_comment.post = post
-            #Сохраняем комментарий в базе данных.
+            # Сохраняем комментарий в базе данных.
             new_comment.save()
     else:
         comment_form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post': post,
-                                                    'comments': comments,
-                                                    'new_comment': new_comment,
-                                                    'comment_form': comment_form})
+    return render(
+        request,
+        "blog/post/detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "new_comment": new_comment,
+            "comment_form": comment_form,
+        },
+    )
